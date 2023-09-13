080b61c2bf2e3dae181434124b629e9f8cb78e89
Pinard Liu <liujianping-ok@163.com>
2023-09-13 21:08:37 +0800

    update dijkstra single return

--- templates/graph/DijkstraHeap1.java
+++ templates/graph/DijkstraHeap1.java
@@ -32,4 +32,31 @@ public class DijkstraHeap1 {
         }
         return distance;
     }
+    public long dijkstra2(int n, int source, int dest, List<int[]>[] g){
+        long[] distance = new long[n];
+        Arrays.fill(distance, 0x3f3f3f3f);
+        distance[source] = 0L;
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b)->{
+            if(a[1] < b[1]) return -1;
+            else if(a[1] > b[1]) return 1;
+            else return 0;
+        });
+        pq.add(new long[]{source, 0L});
+
+        while(pq.size() > 0){
+            long[] tmp = pq.poll();
+            int cur = (int)tmp[0];
+            if(cur == dest) return tmp[1];
+            if(distance[cur] < tmp[1]) continue;
+            for(int[] gCur : g[cur]){
+                int next = gCur[0];
+                int weight = gCur[1];
+                if(distance[next] > distance[cur] + weight){
+                    distance[next] = distance[cur] + weight;
+                    pq.add(new long[]{next, distance[next]});
+                }
+            }
+        }
+        return -1;
+    }
 }
