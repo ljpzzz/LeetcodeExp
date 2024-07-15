9fb170e4992ed294d94b9574bb12f4e4c48fe7be
Pinard Liu <liujianping@black-unique.com>
2024-07-15 15:56:12 +0800

    0644

---
+++ Leetcode0501-1000/Leetcode0644/Leetcode0644_1.java
@@ -0,0 +1,42 @@
+package Leetcode0644;
+
+public class Leetcode0644_1 {
+    public double findMaxAverage(int[] nums, int k) {
+        double right = Integer.MAX_VALUE;
+        double left = Integer.MIN_VALUE;
+        double ans = left;
+        while(right-left > 1e-6){
+            double mid = left + (right-left)/2;
+            if(isOK(nums, k, mid)){
+                ans = mid;
+                left = mid;
+            }
+            else right = mid;
+        }
+        return ans;
+    }
+    public boolean isOK(int[] nums, int k, double mid){
+        int n = nums.length;
+        double[] d = new double[n];
+        for(int i = 0; i < n; i++) d[i] = nums[i]-mid;
+        double preSum = 0d;
+        double sum = 0d;
+        for(int i = 0; i < k; i++) sum += d[i];
+        if(sum > 0) return true;
+        for(int i = k; i < n; i++){
+            sum += d[i];
+            preSum += d[i-k];
+            if(preSum < 0){
+                sum -= preSum;
+                preSum = 0;
+            }
+            if(sum > 0) return true;
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        System.out.println(new Leetcode644().findMaxAverage(new int[]{1,12,-5,-6,50,3}, 4));
+        System.out.println(new Leetcode644().findMaxAverage(new int[]{5}, 1));
+    }
+}
+
